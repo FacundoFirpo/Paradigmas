468c24e7907178826224bee39eb2ce0db7b883ba
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2024-08-16 15:57:18 -0300

    Co-authored-by: Pedro Lonardi Estrada <Pedro-Lonardi@users.noreply.github.com>

--- tp1/fileSystem.hs
+++ tp1/fileSystem.hs
@@ -17,16 +17,21 @@ anunciosF (FS _ anuns) = anuns
 
 agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem            -- permite agregar un anuncio  
 agregarAnuncioF anun fs | elem anun (anunciosF fs) = fs
-                        | otherwise = anun : anunciosF fs
+                        | otherwise = FS (departamentosF fs) (anun : anunciosF fs)
 
 sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem              -- permite eliminar un anuncio
-
+sacarAnuncioF anun fs | notElem anun (anunciosF fs) = error "el anuncio no esta en el file system"
+                      | otherwise = FS (departamentosF fs) nAnuns
+                    where nAnuns = [y| y <- anunciosF fs, y /= anun]
 
 agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem  -- permite agregar un departamento
-agregarAnuncioF dept fs | elem dept (departamentosF fs) = fs
-                        | otherwise = dept : departamentosF fs
+agregarDepartamentoF dept fs | elem dept (departamentosF fs) = fs
+                             | otherwise = FS (dept : departamentosF fs) (anunciosF fs)
 
 sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem    -- permite eliminar un departamento
-
+sacarDepartamentoF dept fs | notElem dept (departamentosF fs) = error "el departamento no esta en el file system"
+                           | otherwise = FS nDepts (anunciosF fs)
+                        where nDepts = [y| y <- departamentosF fs, y /= dept]
 
 anunciosParaF :: [Departamento] -> FileSystem -> [Anuncio]        -- entrega los anuncios a emitir para un conjunto de departamentos
+anunciosParaF depts fs = [anun| anun <- anunciosF fs, aplicaA depts anun]
