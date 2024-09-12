e5909285d7000e21f8762bc86558ac8b8a354794
FacundoFirpo <126919024+FacundoFirpo@users.noreply.github.com>
2024-09-12 14:03:13 -0300

    con ifs

---
+++ tp2/tree/src/tree/EmptyTree.java
@@ -0,0 +1,16 @@
+package tree;
+
+import java.util.List;
+
+public class EmptyTree extends Tree {
+
+    public EmptyTree() {
+        super( null );
+    }
+
+    public Object carga(){
+        return null;
+    }
+
+
+}
--- tp2/tree/src/tree/Tree.java
+++ tp2/tree/src/tree/Tree.java
@@ -1,23 +1,71 @@
 package tree;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Tree {
 
     private Object contenido;
-    private Tree left = new EmptyTree();
-    private Tree right = new EmptyTree();
+    private Tree left = null;
+    private Tree right = null;
 
     public Tree( Object contenido ) {
-        this.contenido = new Node( contenido );
+        this.contenido = contenido;
     }
 
     public List dfs() {
-        return null;
+
+        List visitados = new ArrayList();
+        List pila = new ArrayList();
+        pila.add( this );
+
+        while (!pila.isEmpty()) {
+            Tree vertice = (Tree) pila.removeLast();
+            if (!visitados.contains( vertice )) {
+                visitados.add( vertice );
+                if (vertice.right != null) {
+                    pila.add( vertice.right );
+                }
+                if (vertice.left != null) {
+                    pila.add( vertice.left );
+                }
+            }
+        }
+
+        List dfs = new ArrayList();
+        for (Object vertice : visitados) {
+            dfs.add( ((Tree) vertice).contenido );
+        }
+
+        return dfs;
     }
 
     public List bfs() {
-        return null;
+
+        List visitados = new ArrayList();
+        List cola = new ArrayList();
+        cola.add( this );
+
+        while (!cola.isEmpty()) {
+            Tree vertice = (Tree) cola.remove( 0 );
+            if (!visitados.contains( vertice )) {
+                visitados.add( vertice );
+                if (vertice.left != null) {
+                    cola.add( vertice.left );
+                }
+                if (vertice.right != null) {
+                    cola.add( vertice.right );
+                }
+            }
+        }
+
+        List bfs = new ArrayList();
+        for (Object vertice : visitados) {
+            bfs.add( ((Tree) vertice).contenido );
+        }
+
+        return bfs;
+
     }
 
     public Tree atLeft( Tree leftTree ) {
@@ -31,10 +79,16 @@ public class Tree {
     }
 
     public Tree right() {
+        if (right == null) {
+            throw new RuntimeException( "Nada a la diestra!" );
+        }
         return right;
     }
 
     public Tree left() {
+        if (left == null) {
+            throw new RuntimeException( "Nada a la siniestra!" );
+        }
         return left;
     }
 
